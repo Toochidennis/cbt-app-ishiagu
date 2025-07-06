c9b33fd829f49b93f3e251b698f91e35472e619e
Toochidennis <dennistoochukwu@gmail.com>
2025-07-06 19:20:54 +0100

    pending

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ApolloProvider } from '@apollo/client'
 import '../styles/index.css'
 import App from '../App.tsx'
-import { apolloClient } from '@/services/api/apollo.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <ApolloProvider client={apolloClient}>
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  </ApolloProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
